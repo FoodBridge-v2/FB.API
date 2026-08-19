e85b64f181c8514f31904a39c786c668aa1bd014
ooliveiratg <menezesth16@gmail.com>
2026-08-18 22:47:09 -0300

    initializing the repository

---
+++ src/main/java/br/com/foodbridge/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package br.com.foodbridge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
---
+++ src/test/java/br/com/foodbridge/FoodbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foodbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
